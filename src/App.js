882d927420ea5afafd5b85fdceee4c41a93e0431
Mahir Prcanovic <mahirprcanovic7@gmail.com>
2022-03-31 21:00:44 +0200

    tailwind initialized

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="bg-blue-700 text-cyan-50">Mahir Prcanovic</div>;
 }
 
 export default App;
