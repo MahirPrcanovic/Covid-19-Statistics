a3067677115a0c94df7af915dea42bfd98019f1b
Mahir Prcanovic <mahirprcanovic7@gmail.com>
2022-04-04 15:40:43 +0200

    leaflet initialized, data for country enabled

--- src/components/Home/HomeMain.js
+++ src/components/Home/HomeMain.js
@@ -1,12 +1,80 @@
 import React from "react";
-
+import { MapContainer, TileLayer, Polygon } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { jsonData } from "../Global/data";
+import { statesData } from "../Global/states";
 const HomeMain = () => {
+  const center = [44.282633098132095, 17.785360738692958];
+  // const jsonData = 1;
+  // console.log(jsonData);
   return (
     <div className="bg-red-200 h-screen container mx-auto flex flex-col items-center">
       <h1 className="text-lg text-white font-semibold">
         Most up to date Covid-19 statistics!
       </h1>
-      <div className="w-16 h-16 bg-[url('./images/background.jpg')] bg-center bg-cover"></div>
+      {/* <div className="w-16 h-16 bg-[url('./images/background.jpg')] bg-center bg-cover"></div>
+       */}
+      <MapContainer
+        center={center}
+        zoom={7.4}
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <TileLayer
+          url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=IIIGqRrVE9Zu8srlRf8d"
+          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+        />
+        {jsonData.features.map((feature) => {
+          const coordinates = feature.geometry.coordinates[0].map((coor) => [
+            coor[1],
+            coor[0],
+          ]);
+          statesData.features.map((state) => {
+            const cord = state.geometry.coordinates[0].map((item) => [
+              item[1],
+              item[0],
+            ]);
+            console.log(cord);
+            return <a>Mahir</a>;
+          });
+          return (
+            <Polygon
+              pathOptions={{
+                fillColor: "#FD8D3C",
+                fillOpacity: 0.7,
+                weight: 2,
+                opacity: 1,
+                dashArray: 3,
+                color: "white",
+              }}
+              positions={coordinates}
+              eventHandlers={{
+                mouseover: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    dashArray: "",
+                    fillColor: "#BD0026",
+                    fillOpacity: 0.7,
+                    weight: 2,
+                    opacity: 1,
+                    color: "white",
+                  });
+                },
+                mouseout: (e) => {
+                  const layer = e.target;
+                  layer.setStyle({
+                    fillOpacity: 0.7,
+                    weight: 2,
+                    dashArray: "3",
+                    color: "white",
+                    fillColor: "#FD8D3C",
+                  });
+                },
+                click: (e) => {},
+              }}
+            />
+          );
+        })}
+      </MapContainer>
     </div>
   );
 };
