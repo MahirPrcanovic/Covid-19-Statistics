b0208b41e04766b66d1d97c3a37cf5a6dc64269f
Mahir Prcanovic <mahirprcanovic7@gmail.com>
2022-04-07 22:40:46 +0200

    map all done

--- src/components/Home/HomeMain.js
+++ src/components/Home/HomeMain.js
@@ -1,73 +1,79 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { TileLayer, Polygon } from "react-leaflet";
-import { Popup, Tooltip } from "react-leaflet";
+import { Tooltip } from "react-leaflet";
 import { MapContainer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { data } from "../Global/data2";
 const HomeMain = () => {
-  const center = [44.282633098132095, 17.785360738692958];
+  const center = [43.7390072740577, 17.61093950780978];
 
-  const onEach = () => {};
   return (
-    <div className="bg-red-200 h-screen container mx-auto flex flex-col items-center">
-      <h1 className="text-lg text-white font-semibold">
-        Most up to date Covid-19 statistics!
-      </h1>
-      {/* <div className="w-16 h-16 bg-[url('./images/background.jpg')] bg-center bg-cover"></div>
-       */}
-      <MapContainer center={center} zoom={6.5} className="w-full h-full">
-        <TileLayer
-          url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=IIIGqRrVE9Zu8srlRf8d"
-          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-        />
-        {data.features.map((feature, index) => {
-          const coordinates = feature.geometry.coordinates[0].map((coor) => [
-            coor[1],
-            coor[0],
-          ]);
-
-          return (
-            <Polygon
-              key={index}
-              pathOptions={{
-                fillColor: "#FD8D3C",
-                fillOpacity: 0.7,
-                weight: 2,
-                opacity: 1,
-                dashArray: 3,
-                color: "white",
-              }}
-              positions={coordinates}
-              eventHandlers={{
-                mouseover: (e) => {
-                  const layer = e.target;
-                  layer.setStyle({
-                    dashArray: "",
-                    fillColor: "#BD0026",
-                    fillOpacity: 0.7,
-                    weight: 2,
-                    opacity: 1,
-                    color: "white",
-                  });
-                },
-                mouseout: (e) => {
-                  const layer = e.target;
-                  layer.setStyle({
-                    fillOpacity: 0.7,
-                    weight: 2,
-                    dashArray: "3",
-                    color: "white",
-                    fillColor: "#FD8D3C",
-                  });
-                },
-                click: (e) => {},
-              }}
-            >
-              <Tooltip>Tooltip in Polygon</Tooltip>
-            </Polygon>
-          );
-        })}
-      </MapContainer>
+    <div className="flex">
+      <div className="bg-red-200 h-screen container mx-auto flex flex-col items-center">
+        <MapContainer
+          center={center}
+          zoom={6.5}
+          className="w-full h-1/2 sm:h-full"
+          // scrollWheelZoom={false}
+          dragging={false}
+        >
+          <TileLayer
+            url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=IIIGqRrVE9Zu8srlRf8d"
+            attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+          />
+          {data.features.map((feature, index) => {
+            const coordinates = feature.geometry.coordinates[0].map((coor) => [
+              coor[1],
+              coor[0],
+            ]);
+            console.log(feature);
+            return (
+              <Polygon
+                key={index}
+                pathOptions={{
+                  fillColor: "#FD8D3C",
+                  fillOpacity: 0.7,
+                  weight: 2,
+                  opacity: 1,
+                  dashArray: 3,
+                  color: "white",
+                }}
+                positions={coordinates}
+                eventHandlers={{
+                  mouseover: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      dashArray: "",
+                      fillColor: "#BD0026",
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      opacity: 1,
+                      color: "white",
+                    });
+                  },
+                  mouseout: (e) => {
+                    const layer = e.target;
+                    layer.setStyle({
+                      fillOpacity: 0.7,
+                      weight: 2,
+                      dashArray: "3",
+                      color: "white",
+                      fillColor: "#FD8D3C",
+                    });
+                  },
+                  click: (e) => {},
+                }}
+              >
+                <Tooltip>
+                  <div>Kanton: {feature.properties.name}</div>
+                  <div>Region: {feature.properties.region}</div>
+                </Tooltip>
+              </Polygon>
+            );
+          })}
+          <div className="w-1/2 h-1/2">TEKSt</div>
+        </MapContainer>
+      </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from "react";
-import Header from "../components/Global/Header";
 import HomeMain from "../components/Home/HomeMain";
 
 const Home = () => {
   return (
     <Fragment>
-      <Header />
       <HomeMain />
     </Fragment>
   );
